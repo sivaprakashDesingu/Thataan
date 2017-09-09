35e0059106de11885ea42cc34ec6ff37aaac045d
Manoj Mohan <m.manoj@linuxmail.org>
2017-09-10 03:24:54 +0530

    added validation between practise text and user input text. if validation fails user cant input text

--- src/App.js
+++ src/App.js
@@ -5,21 +5,33 @@ import KeyBoard from './KeyBoard'
 class App extends Component {
   constructor () {
     super()
-    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleInputChangePractiseText = this.handleInputChangePractiseText.bind(this)
+    this.handleInputChangeUserTypedText = this.handleInputChangeUserTypedText.bind(this)
     this.state = { practiseText: '', userTypedText: '' }
   }
 
-  handleInputChange (e) {
+  handleInputChangePractiseText (e) {
     let targetValue = e.target.value
     let targetName = e.target.name
-    this.setState({ [targetName]: targetValue })
+    this.setState({ practiseText: targetValue })
+  }
+
+  handleInputChangeUserTypedText (e) {
+    let userTypedText = e.target.value
+    let practiseText = this.state.practiseText
+    let compareIndex = userTypedText.length - 1
+    if (userTypedText[compareIndex] === practiseText[compareIndex]) {
+      this.setState({ userTypedText: userTypedText })
+    }
   }
   render () {
     return (
       <div>
-        <input type='text' name='practiseText' value={this.state.practiseText} onChange={this.handleInputChange} />
+        <label> Practise Text </ label>
+        <input type='text' name='practiseText' value={this.state.practiseText} onChange={this.handleInputChangePractiseText} />
         <br />
-        <input type='text' name='userTypedText' value={this.state.userTypedText} onChange={this.handleInputChange} />
+        <label> Your Text</ label>
+        <input type='text' name='userTypedText' value={this.state.userTypedText} onChange={this.handleInputChangeUserTypedText} />
         <br />
         <KeyBoard pressedKey={this.state.practiseText[this.state.userTypedText.length]} />
       </div>
