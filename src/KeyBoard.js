8e9614ff322a363b38678f5ec7f6482e68d20342
Manoj Mohan <m.manoj@linuxmail.org>
2017-09-11 14:16:33 +0530

    Create layer1 out of all layer1 keyGroups

--- src/KeyBoard.js
+++ src/KeyBoard.js
@@ -7,52 +7,43 @@ class KeyBoard extends Component {
     let leftLittleFingerGroup = ['ஆ', 'அ', 'ஔ']
     let leftRingFingerGroup = ['ஈ', 'இ', 'ஓ']
     let leftMiddleFingerGroup = ['ஊ', 'உ', 'ஒ']
-    let leftIndexFingerGroup = ['ஐ', '்', 'வ']
+    let leftIndexFingerGroup1 = ['ஐ', '்', 'வ']
     let leftIndexFingerGroup2 = ['ஏ', 'எ', 'ங']
 //	   Right finger groups
-    let rightLittleFingerGroup2 = ['ச','ய']
+    let rightLittleFingerGroup2 = ['ச', 'ய']
     let rightLittleFingerGroup = ['ண', 'ந', 'ழ']
     let rightRingFingerGroup = ['ட', 'த', '.']
     let rightMiddleFingerGroup = ['ன', 'ம', ',']
-    let rightIndexFingerGroup = ['ற', 'ப', 'ர']
+    let rightIndexFingerGroup1 = ['ற', 'ப', 'ர']
     let rightIndexFingerGroup2 = ['ள', 'க', 'ல']
 
+    let layer1 = [
+      leftLittleFingerGroup,
+      leftRingFingerGroup,
+      leftMiddleFingerGroup,
+      leftIndexFingerGroup1,
+      leftIndexFingerGroup2,
+
+      rightIndexFingerGroup2,
+      rightIndexFingerGroup1,
+      rightMiddleFingerGroup,
+      rightRingFingerGroup,
+      rightLittleFingerGroup,
+      rightLittleFingerGroup2
+    ]
+
+    let renderedKeyGroups = layer1.map((keyGroup, key) => {
+      return (
+        <td>
+          <KeyGroup key={keyGroup} pressedKey={this.props.pressedKey} keysInGroup={keyGroup} />
+        </td>
+      )
+    }
+                                                       )
     return (
         <table>
 	    <tbody>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={leftLittleFingerGroup} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={leftRingFingerGroup} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={leftMiddleFingerGroup} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={leftIndexFingerGroup} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={leftIndexFingerGroup2} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={rightIndexFingerGroup2} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={rightIndexFingerGroup} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={rightMiddleFingerGroup} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={rightRingFingerGroup} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={rightLittleFingerGroup} />
-          </td>
-          <td>
-            <KeyGroup pressedKey={this.props.pressedKey} keysInGroup={rightLittleFingerGroup2} />
-          </td>
+            {renderedKeyGroups}
 	    </ tbody>
         </table>
     )
