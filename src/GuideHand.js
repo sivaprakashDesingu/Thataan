de3d1c7c818e7073f71662a5188b517ca4e82805
Manoj Mohan <m.manoj@linuxmail.org>
2017-09-13 12:04:52 +0530

    Add feature to move finger marker based on the group of pressed key

--- src/GuideHand.js
+++ src/GuideHand.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react'
 
 class GuideHand extends Component {
+  getMarkerPosition (groupIdOfPressedKey) {
+    let x = '0px' // left
+    let y = '0px' // top
+    switch (groupIdOfPressedKey) {
+      case 0: // left little finger
+        x = '10px'
+        y = '150px'
+        break
+      case 1: // left ring finger
+        x = '60px'
+        y = '70px'
+        break
+      case 2: // left middle finger
+        x = '140px'
+        y = '30px'
+        break
+      case 3: // left index finger
+        x = '230px'
+        y = '30px'
+        break
+      case 4:
+        x = '290px'
+        y = '30px'
+        break
+      case 5:
+        x = '350px'
+        y = '30px'
+        break
+      case 6:
+        x = '350px'
+        y = '30px'
+        break
+      case 7:
+        x = '435px'
+        y = '30px'
+    }
+    return [x, y]
+  }
   render () {
+    let markerPosition = this.getMarkerPosition(this.props.groupIdOfPressedKey)
     let guideMarkerCss = {
       background: '#f00',
       width: '30px',
       height: '30px',
       borderRadius: '50%',
       position: 'relative',
-      left: '30px',
-      top: '50px'
+      left: markerPosition[0],
+      top: markerPosition[1]
     }
     return (
       <div>
