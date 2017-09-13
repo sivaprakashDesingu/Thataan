2bde0b0cb246a0afe1a484d95f9a2a4d51a139d3
Manoj Mohan <m.manoj@linuxmail.org>
2017-09-13 11:06:50 +0530

    Add GuideMarker

--- src/GuideHand.js
+++ src/GuideHand.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 
 class GuideHand extends Component {
   render () {
+    let guideMarkerCss = {
+      background: '#f00',
+      width: '30px',
+      height: '30px',
+      borderRadius: '50%',
+      position: 'relative',
+      left: '30px',
+      top: '50px'
+    }
     return (
       <div>
         <h1> {this.props.groupIdOfPressedKey} </ h1>
+        <div id='guideMarker' style={guideMarkerCss} />
         <img src={require('./GuideHands.png')} alt='Mountain View' />
       </ div>
     )
