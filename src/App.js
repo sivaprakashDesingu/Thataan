b8a05208e2453fa52860c4509aca88961eccf527
Manoj Mohan <m.manoj@linuxmail.org>
2017-09-19 14:22:29 +0530

    Add Login stubs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 import './App.css'
-import KeyBoard from './KeyBoard'
 import TypingTutor from './TypingTutor'
+import Login from './Login'
 
 class App extends Component {
   render () {
     return (
-      <TypingTutor />
+      <div>
+        <BrowserRouter>
+          <Login />
+        </ BrowserRouter>
+        <TypingTutor />
+      </ div>
     )
   }
 }
